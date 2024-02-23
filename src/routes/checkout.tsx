0641445f86611d6961395ded393f23ceee7baf22
Igor Nicoletti <igornicoletti@outlook.com>
2024-02-23 00:37:20 -0300

    checkout page

--- src/routes/checkout.tsx
+++ src/routes/checkout.tsx
@@ -1,5 +1,141 @@
+import { Navbar } from '../components/navbar'
+import { variantsBase } from '../styles/variants'
+import { BanknotesIcon, BuildingLibraryIcon, CreditCardIcon, MapPinIcon, MinusCircleIcon, PlusCircleIcon, TrashIcon } from '@heroicons/react/24/outline'
+
+const { base, container } = variantsBase()
+
 export function Checkout() {
   return (
-    <h1>Checkout</h1>
+    <div className={base()}>
+      <div className={container()}>
+        <Navbar />
+
+        <div className='w-full flex flex-col xl:flex-row py-16 gap-16 xl:gap-8'>
+          <div className='w-full flex flex-col gap-6'>
+            <h2 className='text-xl font-medium uppercase'>Complete seu pedido</h2>
+            <div className='flex flex-col gap-8'>
+              <div className='w-full flex flex-col gap-8 p-4 md:p-6 rounded border border-dracula-line bg-dracula-line/10'>
+                <div className='flex flex-col md:flex-row items-center md:items-start gap-4'>
+                  <MapPinIcon className='size-6 shrink-0 text-dracula-cyan' aria-hidden='true' />
+                  <div className='flex flex-col text-center md:text-left'>
+                    <p className='text-xl font-medium'>Endereço de entrega</p>
+                    <span>Informe o endereço onde deseja receber o seu pedido.</span>
+                  </div>
+                </div>
+                <div className='grid md:grid-cols-3 gap-4'>
+                  <input type="number" name="CEP" id="" placeholder='CEP'
+                    className='h-12 px-4 py-2 bg-inherit rounded border border-dracula-line hover:border-dracula-foreground focus:border-dracula-foreground focus:outline-none' />
+                  <span className='hidden md:block md:col-span-2'></span>
+                  <input type="text" name="Endereço" id="" placeholder='Endereço'
+                    className='md:col-span-2 h-12 px-4 py-2 bg-inherit rounded border border-dracula-line hover:border-dracula-foreground focus:border-dracula-foreground focus:outline-none' />
+                  <input type="number" name="Número" id="" placeholder='Número'
+                    className='h-12 px-4 py-2 bg-inherit rounded border border-dracula-line hover:border-dracula-foreground focus:border-dracula-foreground focus:outline-none' />
+                  <input type="text" name="Bairro" id="" placeholder='Bairro'
+                    className='h-12 px-4 py-2 bg-inherit rounded border border-dracula-line hover:border-dracula-foreground focus:border-dracula-foreground focus:outline-none' />
+                  <input type="text" name="Cidade" id="" placeholder='Cidade'
+                    className='h-12 px-4 py-2 bg-inherit rounded border border-dracula-line hover:border-dracula-foreground focus:border-dracula-foreground focus:outline-none' />
+                  <input type="text" name="Estado" id="" placeholder='Estado'
+                    className='h-12 px-4 py-2 bg-inherit rounded border border-dracula-line hover:border-dracula-foreground focus:border-dracula-foreground focus:outline-none' />
+                </div>
+              </div>
+              <div className='w-full flex flex-col gap-8 p-4 md:p-6 rounded border border-dracula-line bg-dracula-line/10'>
+                <div className='flex flex-col md:flex-row items-center md:items-start gap-4'>
+                  <BuildingLibraryIcon className='size-6 shrink-0 text-dracula-cyan' aria-hidden='true' />
+                  <div className='flex flex-col text-center md:text-left'>
+                    <p className='text-xl font-medium'>Forma de pagamento</p>
+                    <span>O pagamento é feito na entrega. Escolha a forma que deseja pagar.</span>
+                  </div>
+                </div>
+                <div className='grid md:grid-cols-3 gap-4'>
+                  <button className='flex items-center justify-center gap-2 px-6 h-12 font-medium rounded bg-dracula-line/25 hover:bg-dracula-cyan hover:text-dracula-dark border border-dracula-line active:scale-90 transition ease-in-out duration-300'>
+                    <BanknotesIcon className='size-4' aria-hidden='true' />
+                    <span>Dinheiro</span>
+                  </button>
+                  <button className='flex items-center justify-center gap-2 px-6 h-12 font-medium rounded bg-dracula-line/25 hover:bg-dracula-cyan hover:text-dracula-dark border border-dracula-line active:scale-90 transition ease-in-out duration-300'>
+                    <CreditCardIcon className='size-4' aria-hidden='true' />
+                    <span>Cartão de crédito</span>
+                  </button>
+                  <button className='flex items-center justify-center gap-2 px-6 h-12 font-medium rounded bg-dracula-line/25 hover:bg-dracula-cyan hover:text-dracula-dark border border-dracula-line active:scale-90 transition ease-in-out duration-300'>
+                    <CreditCardIcon className='size-4' aria-hidden='true' />
+                    <span>Cartão de dédito</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='w-full xl:max-w-lg flex flex-col gap-6'>
+            <h2 className='text-xl font-medium uppercase'>Cafés selecionados</h2>
+            <div className='w-full flex flex-col gap-8 p-4 md:p-6 rounded border border-dracula-line bg-dracula-line/10'>
+              <ul className='flex flex-col divide-y divide-dracula-line'>
+                <li className='flex items-center gap-4 py-4'>
+                  <img src='/images/coffee-tradicional.png' alt='Coffee Delivery' className='w-16 hidden md:block' />
+                  <div className='w-full flex flex-col gap-1'>
+                    <div className='flex justify-between gap-6 font-medium text-lg'>
+                      <p>Expresso Tradicional</p>
+                      <p>R$ 9.90</p>
+                    </div>
+                    <div className='flex items-center justify-between gap-6'>
+                      <div className='flex items-center rounded gap-2 border border-dracula-line'>
+                        <button className='p-2 rounded hover:bg-dracula-cyan hover:text-dracula-dark transition ease-in-out duration-300'>
+                          <MinusCircleIcon className='size-4' aria-hidden='true' />
+                        </button>
+                        <p>1</p>
+                        <button className='p-2 rounded hover:bg-dracula-cyan hover:text-dracula-dark transition ease-in-out duration-300'>
+                          <PlusCircleIcon className='size-4' aria-hidden='true' />
+                        </button>
+                      </div>
+                      <button className='hover:text-dracula-red transition ease-in duration-300'>
+                        <TrashIcon className='size-4' aria-hidden='true' />
+                      </button>
+                    </div>
+                  </div>
+                </li>
+                <li className='flex items-center gap-4 py-4'>
+                  <img src='/images/coffee-tradicional.png' alt='Coffee Delivery' className='w-16 hidden md:block' />
+                  <div className='w-full flex flex-col gap-1'>
+                    <div className='flex justify-between gap-6 font-medium text-lg'>
+                      <p>Expresso Tradicional</p>
+                      <p>R$ 9.90</p>
+                    </div>
+                    <div className='flex items-center justify-between gap-6'>
+                      <div className='flex items-center rounded gap-2 border border-dracula-line'>
+                        <button className='p-2 rounded hover:bg-dracula-cyan hover:text-dracula-dark transition ease-in-out duration-300'>
+                          <MinusCircleIcon className='size-4' aria-hidden='true' />
+                        </button>
+                        <p>1</p>
+                        <button className='p-2 rounded hover:bg-dracula-cyan hover:text-dracula-dark transition ease-in-out duration-300'>
+                          <PlusCircleIcon className='size-4' aria-hidden='true' />
+                        </button>
+                      </div>
+                      <button className='hover:text-dracula-red transition ease-in duration-300'>
+                        <TrashIcon className='size-4' aria-hidden='true' />
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <ul className='flex flex-col gap-1'>
+                <li className='flex justify-between gap-4'>
+                  <p>Total dos itens</p>
+                  <p>R$ 19.80</p>
+                </li>
+                <li className='flex justify-between gap-4'>
+                  <p>Entrega</p>
+                  <p>R$ 5.20</p>
+                </li>
+                <li className='flex justify-between gap-4 font-medium text-lg uppercase'>
+                  <p>Valor total</p>
+                  <p>R$ 25.00</p>
+                </li>
+              </ul>
+              <button className='w-full flex items-center justify-center px-6 h-12 font-medium rounded uppercase text-dracula-dark bg-dracula-cyan active:scale-90 transition ease-in-out duration-300'>
+                Confirmar pedido
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
   )
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,260 +1,81 @@
-import { Link } from 'react-router-dom'
-import leite from '/images/coffee-leite.png'
-import latte from '/images/coffee-latte.png'
-import arabe from '/images/coffee-arabe.png'
-import gelado from '/images/coffee-gelado.png'
-import quente from '/images/coffee-quente.png'
-import cubano from '/images/coffee-cubano.png'
-import cremoso from '/images/coffee-cremoso.png'
-import delivery from '/images/coffee-delivery.png'
-import havaiano from '/images/coffee-havaiano.png'
-import irlandes from '/images/coffee-irlandes.png'
-import americano from '/images/coffee-americano.png'
-import capuccino from '/images/coffee-capuccino.png'
-import macchiato from '/images/coffee-macchiato.png'
-import mocaccino from '/images/coffee-mocaccino.png'
-import tradicional from '/images/coffee-tradicional.png'
-import { variantsBase, variantsCoffee, variantsIntro, variantsNavbar } from '../styles/variants'
-import { FireIcon, MinusCircleIcon, PlusCircleIcon, ShoppingBagIcon, ShoppingCartIcon, TruckIcon } from '@heroicons/react/24/outline'
+
+import { Navbar } from '../components/navbar'
+import { filters, products, specifications } from '../data/json'
+import { variantsBase, variantsCoffee, variantsHero } from '../styles/variants'
+import { MinusCircleIcon, PlusCircleIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'
 
 const { base, container } = variantsBase()
-const { navbar, logo, action, carticon, badge } = variantsNavbar()
-const { intro, layout, info, day, time, list, item, icon, web, mobile } = variantsIntro()
-const { coffee, header, ours, filters, filter, wrapper, card, cup, map, rank, description, expresso, subtitle, box, price, amount, controller, btn, minusplus, cart, shopping } = variantsCoffee()
-
-const coffees = [
-  {
-    id: 1,
-    image: tradicional,
-    categories: [
-      { title: 'Tradicional' },
-    ],
-    title: 'Expresso Tradicional',
-    description: 'O tradicional café feito com água quente e grãos moidos.',
-    price: '9.90',
-  },
-  {
-    id: 2,
-    image: americano,
-    categories: [
-      { title: 'Tradicional' },
-    ],
-    title: 'Expresso Americano',
-    description: 'Expresso diluído, menos intenso que o tradicional.',
-    price: '9.90',
-  },
-  {
-    id: 3,
-    image: cremoso,
-    categories: [
-      { title: 'Tradicional' },
-    ],
-    title: 'Expresso Cremoso',
-    description: 'Café expresso tradicional com espuma cremosa.',
-    price: '9.90',
-  },
-  {
-    id: 4,
-    image: gelado,
-    categories: [
-      { title: 'Tradicional' },
-      { title: 'Gelado' },
-    ],
-    title: 'Expresso Gelado',
-    description: 'Bebida preparada com café expresso e cubos de gelo.',
-    price: '9.90',
-  },
-  {
-    id: 5,
-    image: leite,
-    categories: [
-      { title: 'Tradicional' },
-      { title: 'Com leite' },
-    ],
-    title: 'Café com Leite',
-    description: 'Meio a meio de expresso tradicional com leite vaporizado.',
-    price: '9.90',
-  },
-  {
-    id: 6,
-    image: latte,
-    categories: [
-      { title: 'Tradicional' },
-      { title: 'Com leite' },
-    ],
-    title: 'Latte',
-    description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa.',
-    price: '9.90',
-  },
-  {
-    id: 7,
-    image: capuccino,
-    categories: [
-      { title: 'Tradicional' },
-      { title: 'Com leite' },
-    ],
-    title: 'Capuccino',
-    description: 'Bebida com canela feita de doses iguais de café, leite e espuma.',
-    price: '9.90',
-  },
-  {
-    id: 8,
-    image: macchiato,
-    categories: [
-      { title: 'Tradicional' },
-      { title: 'Com leite' },
-    ],
-    title: 'Macchiato',
-    description: 'Café expresso misturado com um pouco de leite quente e espuma.',
-    price: '9.90',
-  },
-  {
-    id: 9,
-    image: mocaccino,
-    categories: [
-      { title: 'Tradicional' },
-      { title: 'Com leite' },
-    ],
-    title: 'Mocaccino',
-    description: 'Café expresso com calda de chocolate, pouco leite e espuma.',
-    price: '9.90',
-  },
-  {
-    id: 10,
-    image: quente,
-    categories: [
-      { title: 'Especial' },
-      { title: 'Com leite' },
-    ],
-    title: 'Chocolate Quente',
-    description: 'Bebida feita com chocolate dissolvido no leite quente e café.',
-    price: '9.90',
-  },
-  {
-    id: 11,
-    image: cubano,
-    categories: [
-      { title: 'Especial' },
-      { title: 'Alcoólico' },
-      { title: 'Gelado' },
-    ],
-    title: 'Cubano',
-    description: 'Drink gelado de café expresso com rum, creme de leite e hortelã.',
-    price: '9.90',
-  },
-  {
-    id: 12,
-    image: havaiano,
-    categories: [
-      { title: 'Especial' },
-    ],
-    title: 'Havaiano',
-    description: 'Bebida adocicada preparada com café e leite de coco.',
-    price: '9.90',
-  },
-  {
-    id: 13,
-    image: arabe,
-    categories: [
-      { title: 'Especial' },
-    ],
-    title: 'Árabe',
-    description: 'Bebida preparada com grãos de café árabe e especiarias.',
-    price: '9.90',
-  },
-  {
-    id: 14,
-    image: irlandes,
-    categories: [
-      { title: 'Especial' },
-      { title: 'Alcoólico' },
-    ],
-    title: 'Irlandês',
-    description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly.',
-    price: '9.90',
-  },
-]
+const { hero, heroBigger, heroHeading, heroIcon, heroLayout, heroMinor, heroSpec, heroSpecs, heroSubtitle, heroTitle } = variantsHero()
+const { coffee, cofAction, cofBtn, cofCard, cofCards, cofCart, cofController, cofCup, cofDescription, cofFilter, cofFilters, cofIcon, cofInfo, cofLayout, cofMain, cofName, cofPrice, cofRank, cofRanks, cofTitle } = variantsCoffee()
 
 export function Root() {
 
   return (
     <div className={base()}>
       <div className={container()}>
-        <div className={navbar()}>
-          <h1 className={logo()}>Coffee Delivery</h1>
-          <Link to='/checkout' className={action()}>
-            <ShoppingCartIcon className={carticon()} aria-hidden='true' />
-            <span className={badge()}>1</span>
-          </Link>
-        </div>
-        <div className={intro()}>
-          <div className={layout()}>
-            <div className={info()}>
-              <h2 className={day()}>Encontre o café perfeito para qualquer hora do dia.</h2>
-              <p className={time()}>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora.</p>
+        <Navbar />
+
+        <div className={hero()}>
+          <div className={heroLayout()}>
+            <div className={heroHeading()}>
+              <h2 className={heroTitle()}>Encontre o café perfeito para qualquer hora do dia</h2>
+              <p className={heroSubtitle()}>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
             </div>
-            <img src={delivery} alt={delivery} className={mobile()} />
-            <ul className={list()}>
-              <li className={item()}>
-                <ShoppingCartIcon className={icon()} aria-hidden='true' />
-                <p>Compra simples e segura.</p>
-              </li>
-              <li className={item()}>
-                <ShoppingBagIcon className={icon()} aria-hidden='true' />
-                <p>Embalagem mantém o café intacto.</p>
-              </li>
-              <li className={item()}>
-                <TruckIcon className={icon()} aria-hidden='true' />
-                <p>Entrega rápida e rastreada.</p>
-              </li>
-              <li className={item()}>
-                <FireIcon className={icon()} aria-hidden='true' />
-                <p>O café chega fresquinho até você.</p>
-              </li>
+            <img src='/images/coffee-delivery.png' alt='Coffee Delivery' className={heroMinor()} />
+            <ul className={heroSpecs()}>
+              {specifications.map(specification => (
+                <li className={heroSpec()} key={specification.id}>
+                  <specification.icon className={heroIcon()} aria-hidden='true' />
+                  <p>{specification.text}</p>
+                </li>
+              ))}
             </ul>
           </div>
-          <img src={delivery} alt={delivery} className={web()} />
+          <img src='/images/coffee-delivery.png' alt='Coffee Delivery' className={heroBigger()} />
         </div>
+
         <div className={coffee()}>
-          <div className={header()}>
-            <h3 className={ours()}>Nossos Cafés</h3>
-            <div className={filters()}>
-              <button className={filter()} type='button'>Tradicional</button>
-              <button className={filter()} type='button'>Especial</button>
-              <button className={filter()} type='button'>Com leite</button>
-              <button className={filter()} type='button'>Alcoólico</button>
-              <button className={filter()} type='button'>Gelado</button>
-            </div>
+          <div className={cofLayout()}>
+            <h3 className={cofTitle()}>Nossos Cafés</h3>
+            <ul className={cofFilters()}>
+              {filters.map(filter => (
+                <li key={filter.id}>
+                  <button className={cofFilter()}>
+                    <span>{filter.text}</span>
+                  </button>
+                </li>
+              ))}
+            </ul>
           </div>
-          <div className={wrapper()}>
-            {coffees.map(coffee => (
-              <div className={card()} key={coffee.id}>
-                <img src={coffee.image} alt={coffee.image} className={cup()} />
-                <ul className={map()}>
-                  {coffee.categories.map((category, index) => (
-                    <li key={index}>
-                      <span className={rank()}>{category.title}</span>
+          <div className={cofCards()}>
+            {products.map(product => (
+              <div className={cofCard()} key={product.id}>
+                <img src={product.src} alt={product.alt} className={cofCup()} />
+                <ul className={cofRanks()}>
+                  {product.ranks.map((rank) => (
+                    <li key={rank.id}>
+                      <span className={cofRank()}>{rank.title}</span>
                     </li>
                   ))}
                 </ul>
-                <div className={description()}>
-                  <p className={expresso()}>{coffee.title}</p>
-                  <p className={subtitle()}>{coffee.description}</p>
+                <div className={cofMain()}>
+                  <p className={cofName()}>{product.title}</p>
+                  <p className={cofDescription()}>{product.description}</p>
                 </div>
-                <div className={box()}>
-                  <p>R$ <span className={price()}>{coffee.price}</span></p>
-                  <div className={amount()}>
-                    <div className={controller()}>
-                      <button className={btn()}>
-                        <MinusCircleIcon className={minusplus()} aria-hidden='true' />
+                <div className={cofInfo()}>
+                  <p>R$ <span className={cofPrice()}>{product.price}</span></p>
+                  <div className={cofAction()}>
+                    <div className={cofController()}>
+                      <button className={cofBtn()}>
+                        <MinusCircleIcon className={cofIcon()} aria-hidden='true' />
                       </button>
-                      <p>1</p>
-                      <button className={btn()}>
-                        <PlusCircleIcon className={minusplus()} aria-hidden='true' />
+                      <p>{product.amount}</p>
+                      <button className={cofBtn()}>
+                        <PlusCircleIcon className={cofIcon()} aria-hidden='true' />
                       </button>
                     </div>
-                    <button className={cart()}>
-                      <ShoppingCartIcon className={shopping()} aria-hidden='true' />
+                    <button className={cofCart()}>
+                      <ShoppingCartIcon className={cofIcon()} aria-hidden='true' />
                     </button>
                   </div>
                 </div>
