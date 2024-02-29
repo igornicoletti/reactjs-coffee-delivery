c3fde510644bc9fc98e627cb3cf928251bc17c40
Igor Nicoletti <igornicoletti@outlook.com>
2024-02-29 17:06:20 -0300

    notification component

--- src/components/notification.tsx
+++ src/components/notification.tsx
@@ -11,11 +11,11 @@ export const Notification = ({ notification }: NotificationProps) => {
     <div aria-live="assertive" className="z-10 pointer-events-none fixed inset-0 flex items-end px-4 py-6 md:p-6 sm:items-start" >
       <div className="flex w-full flex-col items-center sm:items-end">
         <Transition show={notification} as={Fragment} enter="transform ease-out duration-300 transition" enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2" enterTo="translate-y-0 opacity-100 sm:translate-x-0" leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0" >
-          <div className="w-full max-w-sm flex items-start gap-2 p-4 rounded-md shadow-md bg-white">
+          <div className="w-full max-w-md flex items-start gap-2 p-4 rounded-md shadow-md bg-white">
             <CheckCircleIcon className="h-6 w-6 text-dracula-cyan" aria-hidden="true" />
             <div>
               <p className="font-medium text-dracula-dark">Expresso Tradicional adicionado ao carrinho!</p>
-              <p className="text-xs text-dracula-dark">Tradicional café feito com água quente e grãos moidos.</p>
+              <p className="text-xs md:text-sm text-dracula-dark">Tradicional café feito com água quente e grãos moidos.</p>
 
             </div>
           </div>
