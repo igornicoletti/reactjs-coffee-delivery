350be5b8b68eaa1fdc6388a4d9d98ea747a308ef
Igor Nicoletti <igornicoletti@outlook.com>
2024-04-08 12:41:03 -0300

    rename pages and components names

--- src/styles/variants.tsx
+++ src/styles/variants.tsx
@@ -10,13 +10,13 @@ export const RootVariants = tv({
 export const HeaderVariants = tv({
   slots: {
     headerContent: 'w-full flex items-center justify-between py-8',
-    headerTitle: 'text-2xl md:text-3xl font-semibold uppercase text-dracula-cyan hover:animate-pulse transition ease-in-out duration-300',
+    headerTitle: 'text-2xl md:text-3xl font-semibold uppercase text-in-cyan hover:animate-pulse transition ease-in-out duration-300',
     headerCart: 'relative',
-    headerButton: 'flex items-center justify-center p-2 rounded-md bg-dracula-line md:hover:bg-dracula-cyan text-dracula-cyan md:hover:text-dracula-line aria-[current=page]:bg-dracula-cyan aria-[current=page]:text-dracula-line transition ease-in-out duration-300',
+    headerButton: 'flex items-center justify-center p-2 rounded-md bg-in-stone md:hover:bg-in-cyan text-in-cyan md:hover:text-in-stone aria-[current=page]:bg-in-cyan aria-[current=page]:text-in-stone transition ease-in-out duration-300',
     headerIcon: 'size-6 shrink-0',
     headerBadge: 'absolute min-w-5 h-5 -top-2 -right-2',
-    headerPing: 'animate-ping absolute w-full h-full rounded-full bg-dracula-cyan/25',
-    headerQuantity: 'relative flex items-center justify-center text-sm font-bold rounded-full min-w-5 h-5 p-1 border-2 border-dracula-dark text-dracula-line bg-dracula-cyan',
+    headerPing: 'animate-ping absolute w-full h-full rounded-full bg-in-cyan/25',
+    headerQuantity: 'relative flex items-center justify-center text-sm font-bold rounded-full min-w-5 h-5 p-1 border-2 border-in-dark text-in-stone bg-in-cyan',
   }
 })
 
@@ -30,7 +30,7 @@ export const HeroVariants = tv({
     heroItem: 'flex items-center gap-4 md:text-lg',
     heroImageMobile: 'w-full max-w-lg md:hidden mx-auto',
     heroImageDesk: 'w-full max-w-xl lg:max-w-xl xl:max-w-2xl hidden md:block',
-    heroIcon: 'size-6 shrink-0 text-dracula-cyan',
+    heroIcon: 'size-6 shrink-0 text-in-cyan',
   }
 })
 
@@ -41,13 +41,13 @@ export const ProductVariants = tv({
     productTitle: 'text-3xl font-medium uppercase',
     productCard: 'grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6',
     productFilter: 'flex flex-wrap -ml-4 lg:ml-0 lg:-mr-4',
-    productItem: 'flex items-center justify-center first:hidden md:first:flex first px-4 py-2 uppercase cursor-pointer md:hover:text-dracula-cyan transition ease-in-out duration-300',
-    productIcon: 'size-6 shrink-0 text-dracula-cyan -mt-1',
+    productItem: 'flex items-center justify-center first:hidden md:first:flex first px-4 py-2 uppercase cursor-pointer md:hover:text-in-cyan transition ease-in-out duration-300',
+    productIcon: 'size-6 shrink-0 text-in-cyan -mt-1',
   },
   variants: {
     active: {
       true: {
-        productItem: 'font-medium text-dracula-cyan',
+        productItem: 'font-medium text-in-cyan',
       }
     }
   }
@@ -55,18 +55,18 @@ export const ProductVariants = tv({
 
 export const CardVariants = tv({
   slots: {
-    cardContent: 'flex flex-col rounded-md odd:rounded-tr-3xl even:rounded-tl-3xl odd:rounded-bl-3xl even:rounded-br-3xl gap-6 p-4 pt-0 border border-dracula-line bg-dracula-wrapper shadow-md hover:shadow-xl transition ease-in-out duration-300',
+    cardContent: 'flex flex-col rounded-md odd:rounded-tr-3xl even:rounded-tl-3xl odd:rounded-bl-3xl even:rounded-br-3xl gap-6 p-4 pt-0 border border-in-stone bg-in-slate shadow-md hover:shadow-xl transition ease-in-out duration-300',
     cardImage: 'hover:animate-spin w-32 mx-auto -mt-4',
     cardCategory: 'flex items-center justify-center gap-2',
-    cardItem: 'text-xs uppercase px-3 py-1 rounded-full bg-dracula-cyan/5 text-dracula-cyan',
+    cardItem: 'text-xs uppercase px-3 py-1 rounded-full bg-in-cyan/5 text-in-cyan',
     cardDescription: 'flex flex-col text-center gap-2',
     cardTitle: 'text-xl font-medium uppercase',
     cardSubtitle: 'px-4',
     cardInfo: 'flex items-end justify-between gap-2 pt-2 mt-auto',
     cardPrice: 'text-2xl',
     cardAction: 'flex items-center justify-between gap-2',
-    cardQuantity: 'h-10 w-full flex items-center justify-center rounded-md border border-dracula-line hover:border-dracula-white transition ease-in-out duration-300',
-    cardCart: 'h-10 w-10 px-2 flex items-center justify-center rounded-md bg-dracula-line hover:bg-dracula-cyan disabled:hover:bg-dracula-line text-dracula-cyan hover:text-dracula-line disabled:hover:text-dracula-cyan disabled:cursor-not-allowed transition ease-in-out duration-300',
+    cardQuantity: 'h-10 w-full flex items-center justify-center rounded-md border border-in-stone hover:border-in-white transition ease-in-out duration-300',
+    cardCart: 'h-10 w-10 px-2 flex items-center justify-center rounded-md bg-in-stone hover:bg-in-cyan disabled:hover:bg-in-stone text-in-cyan hover:text-in-stone disabled:hover:text-in-cyan disabled:cursor-not-allowed transition ease-in-out duration-300',
     cardIcon: 'size-6 shrink-0',
   }
 })
@@ -78,27 +78,27 @@ export const CartVariants = tv({
     cartSummary: 'w-full xl:max-w-lg flex flex-col gap-8',
     cartTitle: 'text-xl md:text-2xl font-medium uppercase',
     cartPanel: 'flex flex-col gap-8',
-    cartWrapper: 'w-full flex flex-col gap-8 p-4 md:p-6 rounded-md border border-dracula-line bg-dracula-wrapper',
+    cartWrapper: 'w-full flex flex-col gap-8 p-4 md:p-6 rounded-md border border-in-stone bg-in-slate',
     cartHead: 'flex flex-col gap-2',
     cartSubtitle: 'text-xl font-medium',
     cartForm: 'grid md:grid-cols-3 gap-6',
     cartFormHidden: 'hidden md:block md:col-span-2',
     cartFormItem: 'h-12 relative col-span-1',
     cartFormItens: 'h-12 relative col-span-1 md:col-span-2',
-    cartInput: 'w-full h-full px-4 rounded-md peer border border-dracula-line hover:border-dracula-white focus:border-dracula-cyan bg-transparent focus:outline-none invalid:border-dracula-red hover:invalid:border-dracula-red focus:invalid:border-dracula-red transition ease-in-out duration-300',
-    cartLabel: 'absolute top-3 left-px px-4 font-medium bg-dracula-wrapper text-dracula-cyan peer-focus:text-dracula-cyan peer-placeholder-shown:text-dracula-line peer-invalid:text-dracula-red peer-focus:peer-invalid:text-dracula-red scale-75 peer-focus:scale-75 peer-placeholder-shown:scale-100 transform -translate-y-5 peer-focus:-translate-y-5 peer-placeholder-shown:translate-y-0 transition ease-in-out duration-300',
-    cartError: 'absolute -bottom-2 right-2 px-4 bg-dracula-wrapper font-medium text-dracula-orange text-xs',
-    cartPay: 'w-full flex items-center justify-center px-4 h-12 uppercase cursor-pointer rounded-md border border-dracula-cyan bg-transparent ui-checked:bg-dracula-cyan/5 hover:bg-dracula-cyan/5 ui-checked:text-dracula-cyan transition ease-in-out duration-300',
-    cartOrder: 'flex flex-col divide-y divide-dracula-line pb-2',
+    cartInput: 'w-full h-full px-4 rounded-md peer border border-in-stone hover:border-in-white focus:border-in-cyan bg-transparent focus:outline-none invalid:border-in-red hover:invalid:border-in-red focus:invalid:border-in-red transition ease-in-out duration-300',
+    cartLabel: 'absolute top-3 left-px px-4 font-medium bg-in-slate text-in-cyan peer-focus:text-in-cyan peer-placeholder-shown:text-in-stone peer-invalid:text-in-red peer-focus:peer-invalid:text-in-red scale-75 peer-focus:scale-75 peer-placeholder-shown:scale-100 transform -translate-y-5 peer-focus:-translate-y-5 peer-placeholder-shown:translate-y-0 transition ease-in-out duration-300',
+    cartError: 'absolute -bottom-2 right-2 px-4 bg-in-slate font-medium text-in-orange text-xs',
+    cartPay: 'w-full flex items-center justify-center px-4 h-12 uppercase cursor-pointer rounded-md border border-in-cyan bg-transparent ui-checked:bg-in-cyan/5 hover:bg-in-cyan/5 ui-checked:text-in-cyan transition ease-in-out duration-300',
+    cartOrder: 'flex flex-col divide-y divide-in-stone pb-2',
     cartOrderItem: 'flex items-center gap-2 md:gap-4 py-4 first:pt-0',
     cartImage: 'w-12 md:w-16',
     cartInfo: 'w-full flex flex-col gap-1',
     cartBetween: 'flex justify-between gap-2',
     cartDescription: 'font-medium md:text-lg',
     cartAction: 'flex items-center justify-between gap-2',
-    cartTrash: 'md:hover:text-dracula-red transition ease-in duration-300',
+    cartTrash: 'md:hover:text-in-red transition ease-in duration-300',
     cartIcon: 'size-4 shrink-0',
-    cartConfirm: 'w-full flex items-center justify-center px-4 h-12 font-medium uppercase rounded-md bg-dracula-cyan text-dracula-dark disabled:cursor-not-allowed disabled:active:scale-100 active:scale-95 transition ease-in-out duration-300',
+    cartConfirm: 'w-full flex items-center justify-center px-4 h-12 font-medium uppercase rounded-md bg-in-cyan text-in-dark disabled:cursor-not-allowed disabled:active:scale-100 active:scale-95 transform hover:-translate-y-1 transition ease-in-out duration-300',
   },
   variants: {
     active: {
@@ -112,7 +112,7 @@ export const CartVariants = tv({
 export const QuantityVariants = tv({
   slots: {
     quantityContent: 'w-full max-w-28 flex items-center justify-center gap-2',
-    quantityButton: 'px-2 hover:text-dracula-cyan disabled:hover:text-dracula-white disabled:cursor-not-allowed transition ease-in-out duration-300',
+    quantityButton: 'px-2 hover:text-in-cyan disabled:hover:text-in-white disabled:cursor-not-allowed transition ease-in-out duration-300',
     quantityValue: 'w-full min-w-6 text-center font-medium bg-transparent focus:outline-none',
     quantityIcon: 'size-4 shrink-0',
   }
@@ -122,12 +122,12 @@ export const NotifyVariants = tv({
   slots: {
     notifyContent: 'fixed inset-0 flex items-end sm:items-start px-4 py-6 md:p-6 z-10 pointer-events-none',
     notifyPanel: 'w-full flex flex-col items-center sm:items-end',
-    notifyHead: 'w-full max-w-md flex items-center gap-2 px-2 py-4 md:p-4 rounded-md shadow-md bg-white',
-    notifyTitle: 'text-sm md:text-base text-dracula-dark',
+    notifyHead: 'w-full max-w-md flex items-center gap-2 px-2 py-4 md:p-4 rounded-md shadow-lg bg-in-white',
+    notifyTitle: 'text-sm md:text-base text-in-dark',
     notifyCoffee: 'font-medium',
-    notifyCheck: 'size-6 shrink-0 text-dracula-green',
-    notifyExc: 'size-6 shrink-0 text-dracula-orange',
-    notifyEnter: 'transform ease-out duration-300 transition',
+    notifyCheck: 'size-6 shrink-0 text-in-green',
+    notifyExc: 'size-6 shrink-0 text-in-orange',
+    notifyEnter: 'transition ease-out duration-300',
     notifyFrom: 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2',
     notifyEnterTo: 'translate-y-0 opacity-100 sm:translate-x-0',
     notifyLeave: 'transition ease-in duration-100',
@@ -139,15 +139,15 @@ export const NotifyVariants = tv({
 export const ModalVariants = tv({
   slots: {
     modalContent: 'relative z-10',
-    modalBackdrop: 'fixed inset-0 bg-black/25',
+    modalBackdrop: 'fixed inset-0 bg-black/10',
     modalDialog: 'fixed inset-0 overflow-y-auto',
     modalInfo: 'flex min-h-full items-center justify-center text-center p-4',
-    modalPanel: 'w-full max-w-xl flex flex-col items-center transform overflow-hidden rounded-md shadow-md bg-white p-6 align-middle transition-all',
-    modalIcon: 'animate-bounce size-16 shrink-0 text-dracula-cyan',
-    modalTitle: 'text-xl font-medium leading-6 text-dracula-dark mt-2',
-    modalDescription: 'text-dracula-dark mt-4',
-    modalSumary: 'flex flex-col gap-4 font-medium text-sm text-dracula-dark mt-10',
-    modalAction: 'mt-10 w-full flex items-center justify-center px-4 h-12 font-medium uppercase rounded-md bg-dracula-cyan/75 hover:bg-dracula-cyan text-dracula-line transition ease-in-out duration-300',
+    modalPanel: 'w-full max-w-xl flex flex-col items-center transform overflow-hidden rounded-md shadow-lg bg-in-white text-in-dark p-6 align-middle transition-all',
+    modalIcon: 'animate-bounce size-12 shrink-0 text-in-cyan',
+    modalTitle: 'text-xl font-medium leading-6 mt-2',
+    modalDescription: 'mt-4',
+    modalSumary: 'flex flex-col gap-4 font-medium text-sm mt-10',
+    modalAction: 'mt-10 w-full flex items-center justify-center px-4 h-12 font-medium uppercase rounded-md bg-in-cyan text-in-stone transform active:scale-95 hover:-translate-y-1 transition ease-in-out duration-300',
     modalEnter: 'ease-out duration-300',
     modalFrom: 'opacity-0',
     modalEnterTo: 'opacity-100',
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        'dracula-dark': '#282A36',
-        'dracula-white': '#F8F8F2',
-        'dracula-red': '#FF5555',
-        'dracula-cyan': '#8BE9FD',
-        'dracula-line': '#44475A',
-        'dracula-green': '#50FA7B',
-        'dracula-pink': '#FF79C6',
-        'dracula-comment': '#6272A4',
-        'dracula-orange': '#FFB86C',
-        'dracula-purple': '#BD93F9',
-        'dracula-yellow': '#F1FA8C',
-        'dracula-wrapper': '#2B2D39',
+        'in-cyan': '#8BE9FD',
+        'in-dark': '#282A36',
+        'in-green': '#50FA7B',
+        'in-orange': '#FFB86C',
+        'in-red': '#FF5555',
+        'in-slate': '#2B2D39',
+        'in-stone': '#44475A',
+        'in-white': '#F8F8F2',
       }
     }
   },
