a6360b6f49718e011b9476ad155cda01e483b9e8
Igor Nicoletti <igornicoletti@outlook.com>
2024-04-15 02:46:02 -0300

    favico

--- src/data/product.tsx
+++ src/data/product.tsx
@@ -2,6 +2,6 @@ import { ProductProps } from '../types/product'
 import { ProductData } from '../data/product.json'
 
 export const ProductLoader = async (): Promise<ProductProps[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
+  await new Promise((resolve) => setTimeout(resolve, 800))
   return ProductData
 }
