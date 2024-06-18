11303d474aeaa7848ae9ac395b843b71a6aeb3a0
Igor Nicoletti <igornicoletti@outlook.com>
2024-06-18 16:33:55 -0300

    api vercel

--- src/api/product.tsx
+++ src/api/product.tsx
@@ -1,6 +1,4 @@
 import { ProductProps } from '../types'
 
-export const ProductLoader = async (): Promise<ProductProps[]> => {
-  const data = await fetch('http://localhost:3333/products').then((res) => res.json())
-  return data
-}
+export const ProductLoader = async (): Promise<ProductProps[]> =>
+  await fetch('https://api-coffee-delivery-igornicoletti.vercel.app/products').then((res) => res.json())
